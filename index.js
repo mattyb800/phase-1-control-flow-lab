0997a726273c8fb68f958c8286f1e60d8a988867
Matt Brouwer <notjonbonjovi@gmail.com>
2023-04-08 16:24:37 -0400

    test lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet){
+  let scuberGreetingForFeet;
+  if (feet <= 400)
+  return 'This one is on me!';
+  else if (feet > 400, feet <= 2000)
+    return 'That will be twenty bucks.';
+    else if (feet > 2000, feet <= 2500)
+      return 'I will gladly take your thirty bucks.';
+      else if (feet > 2500){
+        return 'No can do.'
+    }
+  }
 
-function ternaryCheckCity(){
   // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(cityName){
+ return cityName === 'NYC'? 'Ok, sounds good.': 'No go.';
+}
+  // Write your code here!
+
+
+function switchOnCharmFromTip(tipAmount){
+  switch (tipAmount){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+
   // Write your code here!
 }
